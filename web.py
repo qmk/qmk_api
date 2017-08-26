01c1cd57852d3c91e6c7c9e98c456e7e421540de
skullY <skullydazed@gmail.com>
2017-08-26 11:16:53 -0700

    Apply some best practices from https://medium.com/studioarmix/learn-restful-api-design-ideals-c5ec915a430f

--- web.py
+++ web.py
@@ -4,8 +4,13 @@ from flask import jsonify, Flask, render_template, request, send_file
 from flask_cors import CORS, cross_origin
 from rq import Queue
 
+if exists('version.txt'):
+    __VERSION__ = open('version.txt').read()
+else:
+    __VERSION__ = '__UNKNOWN__'
+
 app = Flask(__name__)
-cors = CORS(app, resources={'/api/*': {'origins': '*'}})
+cors = CORS(app, resources={'/v*/*': {'origins': '*'}})
 rq = Queue(connection=redis)
 
 # Figure out what keyboards are available
@@ -21,14 +26,24 @@ def error(message, code=400, **kwargs):
 
 ## Views
 @app.route('/', methods=['GET'])
-def index():
-    """FIXME: Make this the documentation for the API.
+def root():
+    """Serve up the documentation for this API.
     """
     return render_template('index.html')
 
 
-@app.route('/api/v1/compile', methods=['POST'])
-def POST_api_v1_compile():
+@app.route('/v1', methods=['GET'])
+def v1():
+    """Serve up the documentation for this API.
+    """
+    return jsonify({
+        'status': 'running',
+        'version': __VERSION__
+    })
+
+
+@app.route('/v1/compile', methods=['POST'])
+def POST_v1_compile():
     """Enqueue a compile job.
     """
     data = request.get_json(force=True)
@@ -36,14 +51,14 @@ def POST_api_v1_compile():
         return error("Invalid JSON data!")
 
     if '/' in data['keyboard'] or '/' in data['subproject'] or '/' in data['keymap']:
-        return error("Fuck off hacker.")
+        return error("Fuck off hacker.", 422)
 
     job = compile_firmware.delay(data['keyboard'], data['subproject'], data['keymap'], data['layers'])
     return jsonify({'enqueued': True, 'job_id': job.id})
 
 
-@app.route('/api/v1/compile/<string:job_id>', methods=['GET'])
-def POST_api_v1_compile_job_id(job_id):
+@app.route('/v1/compile/<string:job_id>', methods=['GET'])
+def POST_v1_compile_job_id(job_id):
     """Fetch the status of a compile job.
     """
     job = rq.fetch_job(job_id)
@@ -62,8 +77,8 @@ def POST_api_v1_compile_job_id(job_id):
     })
 
 
-@app.route('/api/v1/compile/<string:job_id>/hex', methods=['GET'])
-def POST_api_v1_compile_job_id_hex(job_id):
+@app.route('/v1/compile/<string:job_id>/hex', methods=['GET'])
+def POST_v1_compile_job_id_hex(job_id):
     """Download a compiled firmware
     """
     job = rq.fetch_job(job_id)
@@ -75,11 +90,11 @@ def POST_api_v1_compile_job_id_hex(job_id):
         return send_file('firmwares/%s/%s' % (job_id, filename), 'application/octet-stream', as_attachment=True, attachment_filename=filename)
 
     # Send a 400 if we can't find the job or firmware.
-    return error("Compile job not finished or other error.", 404)
+    return error("Compile job not finished or other error.", 422)
 
 
-@app.route('/api/v1/compile/<string:job_id>/src', methods=['GET'])
-def POST_api_v1_compile_job_id_src(job_id):
+@app.route('/v1/compile/<string:job_id>/src', methods=['GET'])
+def POST_v1_compile_job_id_src(job_id):
     """Download a completed compile job.
     """
     if exists('firmwares/%s/qmk_firmware.zip' % job_id):
